39c49fe691e79661ff04088da95df721128b78a1
brianvikonura <vikonura@gmail.com>
2024-06-15 17:40:55 +0700

    feat: Developed admin dashboard, login, and logout functionality

---
+++ resources/views/components/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="py-6 px-6 text-center fixed-bottom">
+    <script>
+        document.write("© ")
+        document.write(new Date().getFullYear())
+    </script>
+    Wedding Organizer JeWePe - <strong>All Right Reserved</strong>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('pages.index');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [DashboardController::class, 'index'])->name('home')->middleware('role:admin');
 });
